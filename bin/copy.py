9a7fdf8149bcaafc7e52d47bb61c5bcc457306e9
erinyoung <eriny@utah.gov>
2025-06-04 17:27:47 -0600

    removed renaming fasta

--- bin/copy.py
+++ bin/copy.py
@@ -1,110 +1,47 @@
 #!/usr/bin/env python3
 import glob
-import csv
 import os
+import shutil
 
-def gfastats_to_dict(header_dict):
-    result = {}
-    with open('gfastats_summary.csv', mode='r') as file:
-        reader = csv.DictReader(file)
-        for row in reader:
-            if row['sample'] == f"{header_dict['name']}_{header_dict['assembler']}":
-                key = row['Header']
-                result[key] = row
-    return result
+def check_circ(file, name):
+    for x in [".fasta", "_reoriented", "_clair3", "_polypolish", "_pypolca"]:
+        name = name.replace(x, "")
 
-def circulocov_to_dict(header_dict):
-    result = {}
-    with open('circulocov_summary.txt', mode='r', newline='') as file:
-        reader = csv.DictReader(file, delimiter='\t')
-        for row in reader:
-            if row['sample'].replace('_reoriented','') == f"{header_dict['name']}_{header_dict['assembler']}" :
-                key = row['contigs']
-                result[key] = row
-    return result
-
-def copy_fasta(fasta, header_dict, gfa_dict, circulocov_dict):
-    with open(fasta, 'r') as file:
-        fasta_dict = {}
-
-        circ_check = any(gfa_dict[contig].get('Is circular') == 'N' for contig in gfa_dict)
-
-        for line in file:
+    with open(file, "r") as f:
+        for line in f:
             line = line.strip()
-            if line.startswith('>'):
-                contig    = str(line.replace('>','').split()[0])
-                circular  = gfa_dict[contig]['Is circular'].replace('Y','true').replace('N','false')
-                length    = gfa_dict[contig]['Total segment length']
-                gc_per    = gfa_dict[contig]['GC content %']
-                meandepth = circulocov_dict[contig]['nanopore_meandepth']
-                assembler = header_dict['assembler']
-                step      = header_dict['step']
-                
-                # creating the header
-                header = f">{contig} circ={circular} len={length} gc={gc_per} cov={meandepth} asmb={assembler}"
-                if assembler != 'unicycler':
-                    header += f" stp={step}"
-                header += "\n"
-                # creating the dict
-                fasta_dict[contig] = {
-                    'seq' : '',
-                    'header' : header,
-                    'length' : int(length)
-                }
-            else:
-                fasta_dict[contig]['seq'] += line
-    
-    sorted_dict = dict(sorted(fasta_dict.items(), key=lambda x: x[1]['length'], reverse = True))
-    with open(f"consensus/{header_dict['fasta']}", 'w') as outfile:    
-        for contig in sorted_dict:
-            seq = '\n'.join([fasta_dict[contig]['seq'][i:i+70] for i in range(0, len(fasta_dict[contig]['seq']), 70)])
-            outfile.write(fasta_dict[contig]['header'])
-            outfile.write(f"seq\n")
+            searching_line = line.split(",")
+            if searching_line[0] == name and searching_line[-1] == "N":
+                return False
+    return True
 
-    if not circ_check:
-        with open(f"consensus/sub_{header_dict['fasta']}", 'w') as outfile:
-            i = 0
-            for contig in sorted_dict:
-                if i < 1:
-                    sub_header = f">{fasta_dict[contig]['header'].split()[0]} [location=chromosome][topology=circular][completeness=complete]\n"
+
+def sub_fasta(fasta):
+    with open(fasta, 'r') as file:
+        i = 0
+        with open(f"consensus/sub_{fasta}", 'w') as outfile:
+            for line in file:
+                line = line.strip()
+                if line.startswith('>') and i < 1:
+                    outfile.write(f">{line.split()[0]} [location=chromosome][topology=circular][completeness=complete]\n")
+                    i += 1
+                elif line.startswith('>') and i >= 1:
+                    outfile.write(f">{line.split()[0]} [plasmid-name=unnamed{i}][topology=circular][completeness=complete]\n")
                 else:
-                    sub_header = f">{fasta_dict[contig]['header'].split()[0]} [plasmid-name=unnamed{i}][topology=circular][completeness=complete]\n"
+                    outfile.write(f"{line}\n")
 
-                i += 1
-
-                outfile.write(sub_header)
-                seq = '\n'.join([fasta_dict[contig]['seq'][i:i+70] for i in range(0, len(fasta_dict[contig]['seq']), 70)])
-                outfile.write(seq + '\n')
 
 def main():
   
-  os.mkdir('consensus')
-  header_dict = {}
-  fasta = glob.glob('*.fasta')[0]
-  header_dict['fasta'] = fasta
-  name = fasta.replace('.fasta', '')
-  assemblers = ['flye', 'myloasm', 'raven', 'unicycler']
-  steps = ['reoriented', 'clair3', 'polypolish', 'pypolca']
+    os.mkdir('consensus')
   
-  for step in steps:
-    if step in name:
-      header_dict['step'] = step
-      name = name.replace(f"_{step}",'')
-      break
+    fasta = glob.glob('*.fasta')[0]
   
-    if 'step' not in header_dict.keys():
-        header_dict['step'] = False
-
-  for assembler in assemblers:
-    if assembler in name:
-      header_dict['assembler'] = assembler
-      name = name.replace(f"_{assembler}",'')
-      break
-
-  header_dict['name'] = name
-  gfa_dict        = gfastats_to_dict(header_dict)
-  circulocov_dict = circulocov_to_dict(header_dict)
-  copy_fasta(fasta, header_dict, gfa_dict, circulocov_dict)
+    shutil.copy(fasta, f"consensus/{fasta}")
+  
+    if check_circ('gfastats_summary.csv', fasta):
+   
+        sub_fasta(fasta)
 
 if __name__ == '__main__':
-  main()
+    main()
